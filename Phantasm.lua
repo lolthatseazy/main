03ed69cb38a69317b72e868bae70fc2c29c1ce7b
ATrain-Roblox <132377436+ATrain-Roblox@users.noreply.github.com>
2024-08-01 06:34:03 +1000

    Update Phantasm.lua

--- Phantasm.lua
+++ Phantasm.lua
@@ -1,1 +1,1 @@
-warn'Santino is after us all....'
+game:GetService("StarterGui"):SetCore("SendNotification", {Title = "(PHANTASM)", Text = "The owner of tsb is onto us, the script is down rn.", Duration = 10})
