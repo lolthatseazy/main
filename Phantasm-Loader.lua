1698b9e68007efac1340edde7d1c4ecb517a1378
Sonic <132377436+ATrainz@users.noreply.github.com>
2024-08-20 23:01:21 +1000

    Update Phantasm-Loader.lua

--- Phantasm-Loader.lua
+++ Phantasm-Loader.lua
@@ -2,4 +2,6 @@ if game.GameId == 3808081382 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/ATrainz/main/main/TSB.lua"))()
 elseif game.GameId == 5868588140 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/ATrainz/main/main/RealmRampage.lua"))()
+else
+  game:GetService("StarterGui"):SetCore("SendNotification", {Title = "(PHANTASM)", Text = "This game is not supported.", Duration = 10})
 end
