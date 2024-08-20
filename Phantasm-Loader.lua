710c0092f0095985fd7d0757e0bebab6d70c16b5
Sonic <132377436+ATrainz@users.noreply.github.com>
2024-08-20 23:00:42 +1000

    Create Phantasm-Loader.lua

---
+++ Phantasm-Loader.lua
@@ -0,0 +1,5 @@
+if game.GameId == 3808081382 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ATrainz/main/main/TSB.lua"))()
+elseif game.GameId == 5868588140 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ATrainz/main/main/RealmRampage.lua"))()
+end
