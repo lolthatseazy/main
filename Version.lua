2a415056ed5fd9ac8186e61115aa5c927f1bbab1
Sonic <132377436+ATrainz@users.noreply.github.com>
2024-09-28 08:21:28 +1000

    Update Version.lua

--- Version.lua
+++ Version.lua
@@ -1,2 +1,2 @@
-game:GetService("StarterGui"):SetCore("SendNotification", {Title = "(PHANTASM) WARNING.", Text = "THE SCRIPT IS DETECTED AS OF A FEW HOURS AGO, IF YOU USE IT, YOU WILL BE BANNED. THIS IS YOUR ONLY WARNING.", Duration = 100})
+--// game:GetService("StarterGui"):SetCore("SendNotification", {Title = "(PHANTASM) WARNING.", Text = "THE SCRIPT IS DETECTED AS OF A FEW HOURS AGO, IF YOU USE IT, YOU WILL BE BANNED. THIS IS YOUR ONLY WARNING.", Duration = 100})
 return true
