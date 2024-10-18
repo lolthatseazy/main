101e036b4302b0c5b3fa3448873e102b3ceef6a0
secret <132377436+ATrainz@users.noreply.github.com>
2024-10-19 09:38:36 +1000

    Create Blacklisted.lua

---
+++ Blacklisted.lua
@@ -0,0 +1,15 @@
+local blacklistedClients = {
+    {Id = "EC2177F6-1254-4FA0-9D6C-6B8D4D9EFA40", Reason = "PHANTASM BAN\nBlacklisted for being trash."}, --// kriptogross1006
+    {Id = "026A6B91-16D3-4857-B786-5E643AB4DE4E", Reason = "PHANTASM BAN\nHello ducks! You have been blacklisted for being an asshole."}, --// ducks (ihave67altslol)
+    {Id = "E9B3A154-8C81-47C9-913B-45F9545ED55C", Reason = "PHANTASM BAN\nYou're a crybaby nilaura."}, --// nilaura (skjdidensuske (AARP Accoustic))
+    {Id = "e86c0b9e-b3ca-4649-91d1-3dc9810a49f1", Reason = "PHANTASM BAN\nDragon told me to."}
+}
+
+local blacklistedHWIDs = {
+    {Id = "1fdf91449626e6832fa3eab0fa7abf549f4cbcf68df117549d6a40f0cff1d2f2", Reason = "PHANTASM BAN\nHello ducks! You have been blacklisted for being an asshole."}, --// ducks (ihave67altslol)
+    {Id = "ca681d5e9cbe4132936975c28002cbe7f6bcf94cd0222b6fcc177f59804d687e", Reason = "PHANTASM BAN\nHello ducks! You have been blacklisted for being an asshole."}, --// ducks (ihave109altslol)
+    {Id = "81ce31a1197fe4768af709913ebffe393ce8acce2d5a69d0740fe272131dac55", Reason = "PHANTASM BAN\nYou're a crybaby nilaura."}, --// nilaura (skjdidensuske (AARP Accoustic))
+    {Id = "3fceb738e2a5791a904bdae020e16e13d6b426361ad921aeb41e8cabcb2c5c9e", Reason = "PHANTASM BAN\nDragon told me to."},
+}
+
+return blacklistedClients, blacklistedHWIDs
