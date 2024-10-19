411bbb97611d4554993d14b1e5b2fb72bebb19b4
secret <132377436+ATrainz@users.noreply.github.com>
2024-10-19 11:18:57 +1000

    Update Phantasm-Loader.lua

--- Phantasm-Loader.lua
+++ Phantasm-Loader.lua
@@ -3,6 +3,6 @@ if game.GameId == 3808081382 then
 elseif game.GameId == 5868588140 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/ATrainz/main/main/RealmRampage.lua"))()
 else
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/ATrainz/main/main/Universal.lua"))()
   game:GetService("StarterGui"):SetCore("SendNotification", {Title = "(PHANTASM)", Text = "This game is not supported, but we can run the beta universal for you.", Duration = 10})
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ATrainz/main/main/Universal.lua"))()
 end
