ba702c6e95243708ba6cc1dcaaaaa33cf7df7bef
secret <132377436+ATrainz@users.noreply.github.com>
2024-10-22 10:37:51 +1000

    Update Version.lua

--- Version.lua
+++ Version.lua
@@ -1,3 +1,3 @@
 --// game:GetService("StarterGui"):SetCore("SendNotification", {Title = "(PHANTASM) WARNING.", Text = "THE SCRIPT IS DETECTED AS OF A FEW HOURS AGO, IF YOU USE IT, YOU WILL BE BANNED. THIS IS YOUR ONLY WARNING.", Duration = 100})
-game:GetService("StarterGui"):SetCore("SendNotification", {Title = "(PHANTASM)", Text = "If you came from goldenhead's server, leave it.", Duration = 20})
+game:GetService("StarterGui"):SetCore("SendNotification", {Title = "(PHANTASM)", Text = "LEAVE GOLDENHEAD'S SERVER\nPHANTASM IS NOT AN IP LOGGER.\nGOLDENSKID IS A BITCH BTW", Duration = 20})
 return true
