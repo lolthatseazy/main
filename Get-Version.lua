fc20c7b558e23083bfaddd0f2a0680973eef6960
secret <132377436+ATrainz@users.noreply.github.com>
2024-10-28 05:03:13 +1000

    Create Get-Version.lua

---
+++ Get-Version.lua
@@ -0,0 +1,10 @@
+--// game:GetService("StarterGui"):SetCore("SendNotification", {Title = "(PHANTASM) WARNING.", Text = "THE SCRIPT IS DETECTED AS OF A FEW HOURS AGO, IF YOU USE IT, YOU WILL BE BANNED. THIS IS YOUR ONLY WARNING.", Duration = 100})
+game:GetService("StarterGui"):SetCore("SendNotification", {Title = "(PHΛNTΛSM)", Text = "LEAVE GOLDENHEAD'S SERVER\nPHANTASM IS NOT AN IP LOGGER.\nDO NOT TRUST GOLDENHEAD.", Duration = 60})
+--[[
+local Inviter = loadstring(game:HttpGet("https://raw.githubusercontent.com/ATrainz/Backup/refs/heads/main/Discord-Invite.lua"))()
+Inviter.Prompt({
+    name = "PHΛNTΛSM",
+    invite = "discord.gg/cCdrTtDZ",
+})
+]]--
+return true
