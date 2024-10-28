9953ebdb84e60546e130c05358c931f7a10606a2
secret <132377436+ATrainz@users.noreply.github.com>
2024-10-29 02:39:19 +1000

    Update Blacklisted.lua

--- Blacklisted.lua
+++ Blacklisted.lua
@@ -15,8 +15,8 @@ local blacklistedHWIDs = {
     {Id = "ca681d5e9cbe4132936975c28002cbe7f6bcf94cd0222b6fcc177f59804d687e", Reason = "PHANTASM BAN\nHello ducks! You have been blacklisted for being an asshole."}, --// ducks (ihave109altslol)
     {Id = "81ce31a1197fe4768af709913ebffe393ce8acce2d5a69d0740fe272131dac55", Reason = "PHANTASM BAN\nYou're a crybaby nilaura."}, --// nilaura (skjdidensuske (AARP Accoustic))
     {Id = "3fceb738e2a5791a904bdae020e16e13d6b426361ad921aeb41e8cabcb2c5c9e", Reason = "PHANTASM BAN\nDragon told me to."},
-    {Id = "64633966333464653535643732346565663866636531323533323739613332636562323862626435383435333438356365636263646139636336623032346562", Reason = "PHANTASM BAN\nLoser"},
-    -- {Id = "4468b632d9204dde6f3118de1f29b9d1affea7f0840ce01c40583393fc229b07", Reason = "PHANTASM BAN\nShitty scripts + you're a skid + you're immature + your ego is too high + goldenheads trash + you leaked my friends early access script + fuck you"},
+    -- {Id = "64633966333464653535643732346565663866636531323533323739613332636562323862626435383435333438356365636263646139636336623032346562", Reason = "PHANTASM BAN\nLoser"},
+    {Id = "4468b632d9204dde6f3118de1f29b9d1affea7f0840ce01c40583393fc229b07", Reason = "PHANTASM BAN\nShitty scripts + you're a skid + you're immature + your ego is too high + goldenheads trash + you leaked my friends early access script + fuck you"},
     {Id = "c30f1d086e7aaeaa8ec95ea89e83fcda09cb25509dda6c7c9b7480cbca683b4a", Reason = "PHANTASM BAN\nYou're not allowed to use this script. skid."},
     {Id = "38306130393763363461373663336366666635333561623565353538623236363265393061353035633566393132366332613565386534363361316132613132", Reason = "PHANTASM BAN\nI made it private for a reason."},
     {Id = "5160cee140ee2b43", Reason = "PHANTASM BAN\nI made it private for a reason."},
