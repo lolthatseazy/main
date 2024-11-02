df550a9974b4cc2ee958879c054c267fbac2e01b
secret <132377436+ATrainz@users.noreply.github.com>
2024-11-03 05:22:19 +1000

    Update Blacklisted.lua

--- Blacklisted.lua
+++ Blacklisted.lua
@@ -3,7 +3,6 @@ local blacklistedClients = {
     {Id = "026A6B91-16D3-4857-B786-5E643AB4DE4E", Reason = "PHANTASM BAN\nHello ducks! You have been blacklisted for being an asshole."}, --// ducks (ihave67altslol)
     {Id = "E9B3A154-8C81-47C9-913B-45F9545ED55C", Reason = "PHANTASM BAN\nYou're a crybaby nilaura."}, --// nilaura (skjdidensuske (AARP Accoustic))
     {Id = "e86c0b9e-b3ca-4649-91d1-3dc9810a49f1", Reason = "PHANTASM BAN\nDragon told me to."},
-    -- {Id = "CD901759-23DC-49DC-8FC9-C4697E959BF1", Reason = "PHANTASM BAN\nLoser"},
     {Id = "12a39237-114d-4fb0-873e-d82703e2f4ed", Reason = "PHANTASM BAN\nShitty scripts + you're a skid + you're immature + your ego is too high + goldenheads trash + you leaked my friends early access script + fuck you"},
     {Id = "8c9fb57c-c325-4d62-a7eb-9bd19c49b47e", Reason = "PHANTASM BAN\nYou're not allowed to use this script. skid."},
     {Id = "BFEA2CE2-8B73-4B55-930E-27E1AB398626", Reason = "PHANTASM BAN\nI made it private for a reason."},
@@ -15,7 +14,6 @@ local blacklistedHWIDs = {
     {Id = "ca681d5e9cbe4132936975c28002cbe7f6bcf94cd0222b6fcc177f59804d687e", Reason = "PHANTASM BAN\nHello ducks! You have been blacklisted for being an asshole."}, --// ducks (ihave109altslol)
     {Id = "81ce31a1197fe4768af709913ebffe393ce8acce2d5a69d0740fe272131dac55", Reason = "PHANTASM BAN\nYou're a crybaby nilaura."}, --// nilaura (skjdidensuske (AARP Accoustic))
     {Id = "3fceb738e2a5791a904bdae020e16e13d6b426361ad921aeb41e8cabcb2c5c9e", Reason = "PHANTASM BAN\nDragon told me to."},
-    -- {Id = "64633966333464653535643732346565663866636531323533323739613332636562323862626435383435333438356365636263646139636336623032346562", Reason = "PHANTASM BAN\nLoser"},
     {Id = "4468b632d9204dde6f3118de1f29b9d1affea7f0840ce01c40583393fc229b07", Reason = "PHANTASM BAN\nShitty scripts + you're a skid + you're immature + your ego is too high + goldenheads trash + you leaked my friends early access script + fuck you"},
     {Id = "c30f1d086e7aaeaa8ec95ea89e83fcda09cb25509dda6c7c9b7480cbca683b4a", Reason = "PHANTASM BAN\nYou're not allowed to use this script. skid."},
     {Id = "38306130393763363461373663336366666635333561623565353538623236363265393061353035633566393132366332613565386534363361316132613132", Reason = "PHANTASM BAN\nI made it private for a reason."},
