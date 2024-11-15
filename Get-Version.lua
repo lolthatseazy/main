989f13eef042fc9b85c724a6da8d4a890bc26fc7
secret <132377436+ATrainz@users.noreply.github.com>
2024-11-16 01:53:48 +1000

    Update Get-Version.lua

--- Get-Version.lua
+++ Get-Version.lua
@@ -3,7 +3,6 @@ local clientId = game:GetService("RbxAnalyticsService"):GetClientId()
 if clientId and clientId == "0A78CFAF-80F5-4CDA-98EF-2ED6DD49CE55" then
     game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Hey", Text = "Cool video on phantasm bro", Duration = 60})
 end
-game:GetService("StarterGui"):SetCore("SendNotification", {Title = "(PHANTASM)", Text = "LEAVE GOLDENHEAD'S SERVER\nPHANTASM IS NOT AN IP LOGGER.\nDO NOT TRUST GOLDENHEAD.", Duration = 60})
 --[[
 local Inviter = loadstring(game:HttpGet("https://raw.githubusercontent.com/ATrainz/Backup/refs/heads/main/Discord-Invite.lua"))()
 Inviter.Prompt({
@@ -11,4 +10,4 @@ Inviter.Prompt({
     invite = "discord.gg/cCdrTtDZ",
 })
 ]]--
-return true
+return false
