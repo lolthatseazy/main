31e3e22fd14cd34c5bdf0e6635895ef5c56dd9e9
lolthatseazy <105562606+lolthatseazy@users.noreply.github.com>
2024-12-01 22:27:36 -0300

    Update Phantasm-Loader.lua

--- Phantasm-Loader.lua
+++ Phantasm-Loader.lua
@@ -1,8 +1,8 @@
 if game.GameId == 3808081382 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/ATrainz/main/main/TSB.lua"))()
+  loadstring(game:HttpGet("https://github.com/lolthatseazy/main/main/TSB.lua"))()
 elseif game.GameId == 5868588140 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/ATrainz/main/main/RealmRampage.lua"))()
+  loadstring(game:HttpGet("https://github.com/lolthatseazy/main/main/RealmRampage.lua"))()
 else
   game:GetService("StarterGui"):SetCore("SendNotification", {Title = "(PHANTASM)", Text = "This game is not supported, but we can run the beta universal for you.", Duration = 10})
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/ATrainz/main/main/Universal.lua"))()
+  loadstring(game:HttpGet("https://github.com/lolthatseazy/main/main/Universal.lua"))()
 end
